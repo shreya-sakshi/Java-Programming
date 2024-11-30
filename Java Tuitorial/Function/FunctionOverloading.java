87bd68363c998bb928048ca3cf72ce6bb08e12e2
Shreya Sakshi <shreyasingh7478@gmaail.com>
2024-11-30 15:04:19 +0530

    Added some Function Overrloading

---
+++ Java Tuitorial/Function/FunctionOverloading.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FunctionOverloading 
+{
+
+    public static int sum(int a , int b)
+    {
+        return a+b;
+    }
+
+    public static int sum(int a,int b,int c)
+    {
+        return a+b+c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(4,5));
+        System.out.println(sum(4,5,6));
+        
+    }
+    
+}
